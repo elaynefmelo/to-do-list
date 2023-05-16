954832ee028de47c4905b97d7ba799f06f8048de
Elayne Melo <elayne.mello6@gmail.com>
2023-05-16 13:00:45 -0300

    mudança cores

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, TextInput, View, TouchableOpacity, FlatList, Keyboard } from 'react-native';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, FlatList, Keyboard, Alert,  AsyncStorage,  } from 'react-native';
 import { AntDesign, MaterialIcons } from '@expo/vector-icons';
+
+
 
 export default function App() {
   const [task, setTask] = useState([]); //guarda a lista de tarefas, por isso um array
   const [newTask, setNewTask] = useState(""); //recebe uma nova tarefa alimentada pelo input
 
   async function addTask() {
+    
+    const search = task.filter(task => task === newTask);
+
+    if(newTask === ''){
+      return;
+    }
+
+    if (search.length !== 0) {
+      Alert.alert("Atenção", "A tarefa ja foi inserida anteriormente!");
+      return;
+    }
+    
     setTask([...task, newTask]);
     setNewTask('');
 
     Keyboard.dismiss();
   }
 
+  async function removeTask(item) {
+    Alert.alert(
+      "Excluir Tarefa",
+      "Tem certeza que deseja excluir essa tarefa?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => {
+            return;
+          },
+          style: "cancel"
+        },
+        {
+          text: "OK",
+          onPress: () => setTask(task.filter(tasks => tasks !== item))
+        }
+      ],
+      { cancelable: false }
+    );
+  }
+ 
   return (
   <>
     <View style={styles.container}>
       <View style={styles.Body}>
-        <Text>TO DO LIST</Text>
+        <Text style={styles.Titulo}>LISTA DE TAREFAS</Text>
       </View>
       <View style={styles.Form}>
         <TextInput 
@@ -41,11 +76,11 @@ export default function App() {
           renderItem={({ item }) => (
             <View style={styles.ContainerView}>
               <Text style={styles.Texto}>{item}</Text>
-              <TouchableOpacity >
+              <TouchableOpacity onPress={() => removeTask(item)}>
                 <MaterialIcons 
                   name='delete-forever'
                   size ={25}
-                  color='#f64c75'
+                  color='#f64'
                 />
               </TouchableOpacity>
             </View>
@@ -60,7 +95,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#fff',
+    backgroundColor:'#CDDFFA',
     paddingHorizontal: 20,
     paddingVertical: 20,
     marginTop: 20,
@@ -93,19 +128,20 @@ const styles = StyleSheet.create({
     width: 40,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#1c6cce',
+    backgroundColor: '#002356',
     borderRadius: 4,
     marginLeft: 10,
   },
   FlatList:{
     flex: 1,
     marginTop: 5,
+
   },
   ContainerView: {
     marginBottom: 15,
     padding: 15,
     borderRadius: 4,
-    backgroundColor: '#eee',
+    backgroundColor: '#FFD24C',
 
     display: 'flex',
     flexDirection: 'row',
@@ -120,7 +156,15 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontWeight: 'bold',
     marginTop: 4,
-  }
+  },
+  Titulo: {
+    fontSize: 18,
+    color: '#002356',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginTop: 25,
+    marginBottom: 15,
+  },
 
   
   }
